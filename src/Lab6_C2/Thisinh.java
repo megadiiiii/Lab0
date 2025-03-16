7aec3c18b729027920dd50741df3151770c3a60a
Mega.D <phamanhduc30012005@gmail.com>
2025-03-17 01:01:01 +0700

    Sửa Lab6_C2 theo ý Mao

--- src/Lab6_C2/Thisinh.java
+++ src/Lab6_C2/Thisinh.java
@@ -6,16 +6,13 @@ public class Thisinh {
     private int id;
     private String name;
     private String address;
-    private float score1, score2, score3, extra;
+    private float extra;
 
     public Thisinh() {}
     public Thisinh(int id, String Name, String address, float score1, float score2, float score3, float extra) {
         this.id = id;
         this.name = Name;
         this.address = address;
-        this.score1 = score1;
-        this.score2 = score2;
-        this.score3 = score3;
         this.extra = extra;
     }
 
@@ -43,40 +40,12 @@ public class Thisinh {
         this.address = address;
     }
 
-    public float getScore1() {
-        return score1;
-    }
-
-    public void setScore1(float score1) {
-        this.score1 = score1;
-    }
-
-    public float getScore2() {
-        return score2;
-    }
-
-    public void setScore2(float score2) {
-        this.score2 = score2;
-    }
-
-    public float getScore3() {
-        return score3;
-    }
-
-    public void setScore3(float score3) {
-        this.score3 = score3;
-    }
-
     public float getExtra() {
         return extra;
     }
 
     public void setExtra(float extra) {
         this.extra = extra;
-    }
-
-    public float totalScore() {
-        return getScore1() + getScore2() + getScore3() + getExtra();
     }
 
     public void input(Scanner sc) {
@@ -87,17 +56,12 @@ public class Thisinh {
         name = sc.nextLine();
         System.out.print("Nhap dia chi: ");
         address = sc.nextLine();
-        System.out.print("Nhap diem mon 1: ");
-        score1 = sc.nextFloat();
-        System.out.print("Nhap diem mon 2: ");
-        score2 = sc.nextFloat();
-        System.out.print("Nhap diem mon 3: ");
-        score3 = sc.nextFloat();
         System.out.print("Nhap diem uu tien: ");
         extra = sc.nextFloat();
+        sc.nextLine();
     }
 
     public String toString() {
-        return "SBD: " + getId() + ", Ho ten: " + getName() + ", Dia chi: " + getAddress() + "\nM1: " + getScore1() + ", M2: " + getScore2() + ", M3: " + getScore3() + ", UT: " + getExtra() + "\nTong diem: " + totalScore() + "\n";
+        return "SBD: " + getId() + ", Ho ten: " + getName() + ", Dia chi: " + getAddress() + ", UT: " + getExtra() + "\n";
     }
 }
